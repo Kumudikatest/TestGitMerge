f7b6f5b79c4893573184ff81af5c5ba89465b92a
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-06-25 13:19:57 +0530

    k1

---
+++ TestGitMerge/lambda.js
@@ -0,0 +1,19 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_D10y3fy0o",
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            cosole.log(error);
+            throw error;
+        }
+        else {
+            console.log(data);
+        }
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
