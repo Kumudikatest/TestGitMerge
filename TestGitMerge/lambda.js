091241bc990c94c6b9928212a655d861b344d86e
HirudineeADRT <hirudinee@adroitlogic.com>
2019-07-01 16:58:03 +0530

    hiru - 001

--- TestGitMerge/lambda.js
+++ TestGitMerge/lambda.js
@@ -1,12 +1,13 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
     cognito_idp.listUsers({
         UserPoolId: "us-east-1_D10y3fy0o",
-        AttributesToGet: ["sub", "name"],
-        Filter: "eeeee",
-        Limit: 121
+        AttributesToGet: ["sub", "name", "family_name", "nickname"],
+        Filter: "Hiru",
+        Limit: 1222
     }, function (error, data) {
         if (error) {
             cosole.log(error);
@@ -15,6 +16,7 @@ exports.handler = function (event, context, callback) {
         else {
         }
     });
+
 
     callback(null, { "message": "S" });
 }
